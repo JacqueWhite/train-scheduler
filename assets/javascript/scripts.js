18df71ac4c9c05991ae530a623b062cd712765ab
JacqueWhite <jacquecwhite@gmail.com>
2017-08-24 17:03:36 -0500

    fixed table info push

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,3 +1,17 @@
+//Clock for HTML Page
+
+function updateClock() {
+
+var clock = moment().format("h:mm:ss a");
+
+var c = $("<h2>");
+var c2 = c.append(clock);
+$("#clock").html(c2);
+
+};
+
+setInterval(updateClock, 1000);
+
   // Initialize Firebase
 var config = {
     apiKey: "AIzaSyDBoEuZy2xHWovVi1DlD99ad14R6mFk0kA",
@@ -17,8 +31,6 @@ var newDest = "";
 var firstTrain = "";
 var newFreq = "";
 var newTrain = "";
-var nextArrive = 0;
-var minutesAway = 0;
 
 // capture button click
 $("#submit-button").on("click", function(event) {
@@ -29,36 +41,6 @@ firstTrain = moment($("#new-time").val().trim(), "hh:mm a").format("HH:mm");
 newFreq = $("#new-freq").val().trim();
 newTrain = $("#new-name").val().trim();
 
-
-//calculating the next train and min away
-// First Time (pushed back 1 year to make sure it comes before current time)
-  var firstTimeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
-
-
-  // Difference between the times
-  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-
-
-  // Time apart (remainder)
-  var tRemainder = diffTime % newFreq;
-
-
-  // Minute Until Train
-  var minutesAway = newFreq - tRemainder;
-
-
-  // Next Train
-  var nextArrive = moment().add(minutesAway, "minutes");
-
-
-  // Arrival time
-  var arriveTime = moment(nextArrive).format("hh:mm a");
-
-  var nextArrivalUpdate = function() {
-    date = moment(new Date())
-    datetime.html(date.format('hh:mm a'));
-  }
-
 //handling the data push
 
 dataRef.ref().push({
@@ -68,7 +50,6 @@ dataRef.ref().push({
 	newFreq: newFreq,
 	newDest: newDest,
 	dateAdded: firebase.database.ServerValue.TIMESTAMP
-	// dateAdded: firebase.database.ServerValue.TIMESTAMP 
 });
 // alert("Form submitted!");
 
@@ -80,21 +61,41 @@ dataRef.ref().push({
 
 
 dataRef.ref().on("child_added", function(childSnapshot) {
+    // console.log(childSnapshot.val());
+  var newTrain = childSnapshot.val().newTrain;
+  var firstTrain = childSnapshot.val().firstTrain;
+  var newFreq = childSnapshot.val().newFreq;
+  var newDest = childSnapshot.val().newDest;
 
-	console.log(childSnapshot.val().newTrain);
-    console.log(childSnapshot.val().firstTrain);
-    console.log(childSnapshot.val().newFreq);
-    console.log(childSnapshot.val().newDest);
-  
+//calculating the next train and min away
+// First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
 
-});
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % newFreq;
+
+  // Minute Until Train
+  var minutesAway = newFreq - tRemainder;
+
+  // getting next arrival time
+  var nextArrival = moment().add(minutesAway, "minutes");
+
+  var nextArrival2 = moment(nextArrival).format("hh:mm");
+
+  var nextArrivalUpdate = function() {
+    date = moment(new Date())
+    datetime.html(date.format('hh:mm a'));
+  }
 
 // adding the new train to the table
 
 $("#train-sched > tbody").append("<tr><td>" + newTrain + "</td><td>" 
-+ newDest + "</td><td>" + newFreq + "</td><td>" +  
-+ "</td><td>" + nextArrive + "</td></td>" +  
-+ "</td><td>" + arriveTime + "</td></tr>");
++ newDest + "</td><td>" + newFreq + "</td><td>" + nextArrival2 + "</td><td>" 
++ minutesAway + "</td></tr>");
+
+  });
 
 });
-
